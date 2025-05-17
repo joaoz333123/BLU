bd68d3a43d41abc10205dc2c32cbc247d1ad27bc
joaoz333123 <joaozanetti3@gmail.com>
2025-05-17 02:36:58 -0300

    update app.py para robots e sitemap

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
+# Página principal
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# Página de teste
 @app.route("/teste")
 def teste():
     return render_template("index_teste.html")
 
+# Servir sitemap.xml
+@app.route('/sitemap.xml')
+def sitemap():
+    return send_from_directory('.', 'sitemap.xml')
+
+# Servir robots.txt
+@app.route('/robots.txt')
+def robots():
+    return send_from_directory('.', 'robots.txt')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
